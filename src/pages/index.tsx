f60e659d4dedd33eb116f6e54f40c3d8da7007cc
Felipe Yui <felipe_m87@yahoo.com.br>
2021-12-10 16:58:05 +1030

    project created

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+import styles from '@/styles/Home.module.scss';
+
+export default function Home() {
+  return (
+  <>
+    <h1>Felipe</h1>
+  </>
+  );
+}
